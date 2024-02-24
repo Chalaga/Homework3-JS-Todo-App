44bede7ff72d9bcb9a970536c11e0bd9b3c66846
Chalaga <150872963+Chalaga@users.noreply.github.com>
2024-02-24 21:22:02 +0400

    Added and fixed java Script

--- App.js
+++ App.js
@@ -15,50 +15,76 @@ input.addEventListener("keydown", (event) => {
 
 let tasks = [];
 
+
+window.addEventListener("load", () => {
+  tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  tasks.forEach((task) => {
+    createTaskElement(task);
+  });
+});
+
 function Addeditems() {
   if (input.value === "") {
     alert("Input cannot be Empty");
-  } else {
-    const content = `
-        <div class="listcontainer list1">
-        <div class="listImgAndValueContainer">
-            <img class="listimg" src="./Assets/Check.svg">
-        <h1>${input.value}</h1>
-        </div>
-        <img class="deletesvg" src="./Assets/Delete.svg">
-      </div>`;
-    const newItem = document.createElement("div");
-    newItem.innerHTML = content;
-    tasks.push(input.value);
-
-    listContainer.insertBefore(newItem, footer);
-
-    const imgElement = newItem.querySelector(".listimg");
-    let isChecked = false;
-
-    const Deleteoption = newItem.querySelector(".deletesvg");
-
-    imgElement.addEventListener("click", () => {
-      if ((isChecked = !isChecked)) {
-        imgElement.src = isChecked
-          ? "./Assets/Checked.svg"
-          : "./Assets/Check.svg";
-        newItem.style.textDecorationLine = "line-through";
-      } else {
-        imgElement.src = isChecked
-          ? "./Assets/Check.svg"
-          : "./Assets/Check.svg";
-        newItem.style.textDecorationLine = "none";
-      }
-    });
-
-    Deleteoption.addEventListener("click", () => {
-      if (newItem.style.textDecorationLine !== "line-through") {
-        alert("Undone list cannot be deleted");
-      } else {
-        newItem.remove();
-      }
-    });
-    input.value = "";
+    return;
   }
+
+  const task = input.value;
+  createTaskElement(task);
+  tasks.push(task);
+  saveToLocalStorage();
+
+  input.value = "";
+}
+
+function createTaskElement(task) {
+  const content = `
+    <div class="listcontainer list1">
+      <div class="listImgAndValueContainer">
+        <img class="listimg" src="./Assets/Check.svg">
+        <h1>${task}</h1>
+      </div>
+      <img class="deletesvg" src="./Assets/Delete.svg">
+    </div>`;
+
+  const newItem = document.createElement("div");
+  newItem.innerHTML = content;
+
+  listContainer.insertBefore(newItem, footer);
+
+  const imgElement = newItem.querySelector(".listimg");
+  imgElement.addEventListener("click", () => toggleItemCompletion(newItem));
+
+  const deleteOption = newItem.querySelector(".deletesvg");
+  deleteOption.addEventListener("click", () => deleteItem(newItem, task));
+}
+
+function toggleItemCompletion(item) {
+  const imgElement = item.querySelector(".listimg");
+  const isChecked = imgElement.src.includes("Checked");
+
+  imgElement.src = isChecked ? "./Assets/Check.svg" : "./Assets/Checked.svg";
+  item.style.textDecorationLine = isChecked ? "none" : "line-through";
+
+  saveToLocalStorage();
+}
+
+function deleteItem(item, task) {
+  const isCompleted = item.style.textDecorationLine === "line-through";
+  if (!isCompleted) {
+    alert("Undone list cannot be deleted");
+    return;
+  }
+
+  item.remove();
+
+  const index = tasks.indexOf(task);
+  if (index !== -1) {
+    tasks.splice(index, 1);
+    saveToLocalStorage();
+  }
+}
+
+function saveToLocalStorage() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
